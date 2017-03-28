c10cc753293cd63a9f7616405b083759a20a1ba9
Angel Veloz <vlzangel91@gmail.com>
2017-03-28 17:58:26 +0200

    Ajuste en busqueda

--- wp-content/plugins/kmimos/shortcodes/kmimos_search.php
+++ wp-content/plugins/kmimos/shortcodes/kmimos_search.php
@@ -1,39 +1,29 @@
 <?php
-
     //[kmimos_search]
-
     $pais = (isset($args["pais"]))?$args["pais"]:"mx";
-
     $vacios = (isset($args["vacios"]))?$args["vacios"]:1;
-
     $taxonomy = 'pointfinderlocations';
     $parent = get_term_by('slug', $pais, $taxonomy);
     $distdef = 20;
-
     global $wpdb;
     
     $estados = $wpdb->get_results("SELECT * FROM states WHERE country_id = 1 ORDER BY name ASC");
-
     $str_estados = "";
     foreach($estados as $estado) { 
         $str_estados .= "<option value='".$estado->id."'>".$estado->name."</option>";
     }
     $str_estados = utf8_decode($str_estados);
-
     $json = array();
     foreach ($estados as $estado) {
         
         $municipios = $wpdb->get_results("SELECT * FROM locations WHERE state_id = {$estado->id} ORDER BY name ASC");
-
         foreach ($municipios as $municipio) {
             $json[$estado->id][] = array(
                 "id" => $municipio->id,
                 "name" => $municipio->name
             );
         }
-
     }
-
     echo "<script> var temp = eval( '(".json_encode($json).")' ); var locaciones = jQuery.makeArray( temp ); </script>"; 
 ?>
 
@@ -41,29 +31,23 @@
         #estado_cuidador_main {
             margin: 0px !important;
         }
-
         #grupo_fecha {
             width: 110% !important;
         }
-
         .selector_fecha {
             width: 50%;
             overflow: hidden;
         }
-
         .grupo_fecha {
             overflow: hidden;
         }
-
         .grupo_selector {
             display: inline-block;
             width: 50%;
             float: left;
             margin-top: 3px;
         }
-
         html.iOS .fecha_hasta, html.iOS .fecha_desde { width: 118px !important}
-
         @media only screen and (max-width: 639px) { 
             .grupo_selector { width: 100%; } 
             html.iOS .selector_fecha {width: 50% !important}
@@ -73,14 +57,12 @@
             .fecha_hasta { display: block;}
             .fecha_hasta_full { display: none;}
         }
-
         .grupo_selector .marco {
             display: inline-block;
             border: 1px solid #888;
             width: 99%;
             padding-right: 5px;
         }
-
         .grupo_selector .icono {
             display: inline-block;
             width: 36px;
@@ -89,12 +71,10 @@
             padding: 5px;
             font-size: 1.8em;
         }
-
         .grupo_selector sub {
             top: 8px;
             float: left;
         }
-
         .grupo_selector select, .grupo_selector input {
             height: 25px;
             width: 87%;
@@ -106,7 +86,6 @@
             background-color: #ffffff;
         }
         select.activo, .grupo_fecha input[type=date].activo { color: #00d2b7 !important; }
-
         select:focus{
             outline: 0px;
         }
@@ -134,7 +113,6 @@
                         "paseos"                     => '<p>Paseos<br><sup></sup></p>',
                         "adiestramiento"             => '<p>Entrenamiento<br><sup></sup></p>'
                     ); 
-
                     foreach($servicios as $key => $value){ ?>
                         <div class="contenedor_servicio izquierda text-left">
                             <div class="boton_portada boton_servicio">
@@ -182,7 +160,6 @@
                                     'icon' => 'acupuntura'
                                 )
                             );
-
                             foreach($extras as $key => $value){ ?>
                                 <div class="w96pc boton_extra text-center">
                                     <div class="boton_portada boton_servicio">
@@ -221,7 +198,7 @@
                                 </div>
                                 <sub>Municipio:</sub><br>
                                 <select id="estado_cuidador" name="estados" data-location="co">
-                                    <option value="">Seleccione un municipio</option>
+                                    <option value="">Seleccione una ciudad</option>
                                     <?php echo $str_estados; ?>
                                 </select>
                             </div>
@@ -234,7 +211,7 @@
                                 </div>
                                 <sub>Localidad:</sub><br>
                                 <select id="municipio_cuidador" name="municipios">
-                                    <option value="">Seleccione primero un municipio</option>
+                                    <option value="">Seleccione primero una ciudad</option>
                                 </select>
                             </div>
                         </div>
@@ -269,7 +246,6 @@
                             "grandes"  => "Grandes <br><sub>58.0 cm - 73.0 cm</sub>",
                             "gigantes" => "Gigantes <br><sub>73.0 cm - 200.0 cm</sub>",
                         );
-
                         foreach($tamanos as $key=>$value){ ?>
                             <div class="jj_btn_tamanos" style="float: left; box-sizing: border-box; padding: 0px 1px; margin-bottom: 2px !important;">
                                 <div class="boton_portada boton_servicio" style="margin: 0px !important;">
@@ -299,14 +275,12 @@
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script type="text/javascript">
     var hasGPS=false;
-
     (function($) {
         "use strict";
         $.pfsliderdefaults = {};
         $.pfsliderdefaults.fields = Array();
         var urlSever = '<?php echo get_home_url(); ?>/wp-content/plugins/kmimos/app-server.php';
         var isTouchDevice = 'ontouchstart' in document.documentElement;
-
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(coordenadas);
         } else {
@@ -314,42 +288,33 @@
             $("#selector_coordenadas").addClass("hide");
             $("#selector_tipo").addClass("hide");
         }
-
         if(navigator.platform.substr(0, 2) == 'iP') $('html').addClass("iOS");
-
         $(function(){
             var pais = $("#estado_cuidador").attr("data-location");
             var edos = $("#estado_cuidador");
             var mpos = $("#municipio_cuidador");
-
             if($("#otra-localidad").prop( "checked" )){
                 $("#selector_locacion").removeClass("hide");
                 $("#selector_coordenadas").addClass("hide");
                 $('#estado_cuidador > option[value="'+pais+'"]').first().attr('selected', 'selected');
             }
-
             $("#otra-localidad").click(function(){
                 $("#selector_locacion").removeClass("hide");
                 $("#selector_coordenadas").addClass("hide");
             });
-
             $("#mi-ubicacion").click(function(){
                 $("#selector_coordenadas").addClass("hide");
                 $("#selector_locacion").addClass("hide");
             });
-
             edos.change(function(){
                 vlz_ver_municipios();
             });
-
             mpos.change(function(){
                 vlz_coordenadas();
             });
-
             var toRadian = function (deg) {
                 return deg * Math.PI / 180;
             };
-
             function vlz_ver_municipios(){
                 var id =  jQuery("#estado_cuidador").val();
                 var txt = jQuery("#estado_cuidador option:selected").text();
@@ -364,7 +329,6 @@
                     jQuery("#municipio_cuidador").html("<option value=''>Seleccione una ciudad primero</option>");
                 }
             }
-
             function vlz_coordenadas(){
                 var estado = jQuery("#estado_cuidador option:selected").text();
                 var municipio_val = jQuery("#municipio_cuidador option:selected").val();
@@ -373,7 +337,7 @@
                 if( estado != "" ){ 
                     adress+="+"+estado; 
                 }
-                if( municipio != "" ){ 
+                if( municipio_val != "" ){ 
                     adress+="+"+municipio; 
                 }
                 jQuery.ajax({ 
@@ -390,7 +354,6 @@
                     }
                 });
             } 
-
             function calcular_rango_de_busqueda(norte, sur){
                 var d = ( 6371 * 
                     Math.acos(
@@ -414,27 +377,23 @@
                 );
                 return d;
             }
-
             $(".boton_servicio > input:checkbox").each(function(index){
                 var servicio = $(this).attr('data-key');
                 var activo = $(this).prop('checked');
                 if(activo) $(this).parent().addClass('activo');
                 else $(this).parent().removeClass('activo');
             });
-
             $(".boton_portada > input:checkbox").on('change',function(e){
                 var servicio = $(this).attr('data-key');
                 var activo = $(this).prop('checked');
                 if(activo) $(".servicio_cuidador_"+servicio).parent().addClass('activo');
                 else $(".servicio_cuidador_"+servicio).parent().removeClass('activo');
             });
-
             $(".modal").fancybox({
                 maxWidth: 340
             });
         });
     })(jQuery);
-
     function coordenadas(position){
         if(position.coords.latitude!='' && position.coords.longitude!='') {
             document.getElementById("latitud").value=position.coords.latitude;
@@ -445,4 +404,3 @@
         }
     }
 </script>
-
