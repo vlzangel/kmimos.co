d268bb405610a999a720c55645beb9756fd37aa1
etpro01 <etpro@etprogroup.com>
2017-08-15 18:54:23 -0400

    remove colombia

--- wp-content/plugins/kmimos/vlz_order_funciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/* Detalles del servicio */
-
-		$detalles = kmimos_desglose_reserva($o, true);
-
-		$msg_id_reserva = $detalles["msg_id_reserva"];
-		$aceptar_rechazar = $detalles["aceptar_rechazar"];
-		$detalles_servicio = $detalles["detalles_servicio"];
-?>
