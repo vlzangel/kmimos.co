ee92fc036cfd5efa5414bb8c1135696737eb57aa
etpro01 <etpro@etprogroup.com>
2017-09-11 10:41:50 -0400

    sacar alejandra lazaro

--- wp-content/plugins/kmimos/angel.php
+++ wp-content/plugins/kmimos/angel.php
@@ -107,13 +107,13 @@ if(!function_exists('kmimos_mails_administradores_new')){
         $email_admin = $info["email"];
 
         $headers_admins = array(
-        //     'BCC: e.celli@kmimos.la',
-             'BCC: a.lazaro@kmimos.la',
+             //'BCC: e.celli@kmimos.la',
+             //'BCC: a.lazaro@kmimos.la',
              'BCC: r.cuevas@kmimos.la',
              'BCC: r.gonzalez@kmimos.la',
              'BCC: s.cedeno@kmimos.la',
              'BCC: n.deligny@kmimos.la',
-        //     'BCC: m.castellon@kmimos.la',
+             //'BCC: m.castellon@kmimos.la',
              'BCC: a.pedroza@kmimos.la'
         );
 
